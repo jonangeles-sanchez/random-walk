496ce90cddfc8206dfbd100975510d5bedb1391b
Jonathan Angeles <70487639+jonangeles-sanchez@users.noreply.github.com>
2022-10-11 19:11:37 -0400

    Update randomWalkFunctions.php

--- randomWalkFunctions.php
+++ randomWalkFunctions.php
@@ -1,6 +1,4 @@
-<?php 
-
-
+<?php
 /*********************************************************************************
 Function to decide which direction we should go next based on a randomly generated
 number using 'rand'
@@ -42,6 +40,7 @@ function checkDirections($row, $col, $size, $grid){
     return $taken;
 }
 
+
 /*********************************************************************************
 Function to fill the grid with dots (periods)
  ********************************************************************************/
@@ -54,89 +53,81 @@ function initializeGrid($size){
        } 
      return $grid;  
 }
-
-
+   
 /*********************************************************************************
 Function to display the final grid
  ********************************************************************************/
 function printGrid($size, $grid){
-echo "<pre>";
-  for($i=0;$i<$size;$i++){
-      for($j=0;$j<$size;$j++)
-       echo $grid[$i][$j] . " ";
-      printf("\n");
-    }
-echo "</pre>";
-}
-
-
-
-/*********************************************************************************
-Function generates a random starting point and builds path
- ********************************************************************************/
-function generateRandomPoint($seed, $walkLength, $gridSize) {
-    echo "Seed = " . $seed . "\n";
-
-    $first=true;
-
-      /*Generate a random starting point*/
-      $row=mt_rand()%$gridSize;
-      $col=mt_rand()%$gridSize; 
-
-      $alphabet=str_split("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-
-      $grid[$row][$col]=$alphabet[0];//set first 'A' in grid
-
-      for($n=0;$n<$walkLength;$n++){//for as many times as they want the alphabet repeated
-
-          for($letter=0;$letter<26;$letter++){//make a path with the alphabet
-        
-    	  if($first){//if this is the first 'A' of all
-    	      $first=false;
-    	      $letter=1;
-    	    }
-    	  /*****check whether all sides are blocked******/
-    	  $taken=checkDirections($row, $col, $gridSize, $grid);
-
-    	  /*if they are, print what we have and exit*/
-    	  if(allTaken($taken)){//spans through to next php block
-    	      printGrid($gridSize, $grid);
-          } else {
-            exit(0);
-          }
+    /*
+    echo "<pre>";
+    for($i=0;$i<$size;$i++){
+        for($j=0;$j<$size;$j++)
+        echo $grid[$i][$j] . " ";
+        printf("\n");
         }
+    echo "</pre>";
+    */
+    echo "<table>";
+    for($i=0;$i<$size;$i++){
+        echo "<tr>";
+        for($j=0;$j<$size;$j++){
+            echo "<td>" . $grid[$i][$j] . "</td>";
+        }
+        echo "</tr>";
     }
-      
 }
-
 
 /*********************************************************************************
-Function gets the next direction 
- ********************************************************************************/
-function getDirection($taken, $row, $col, $alphabet, $letter){
-    $next=getNextDirection($taken);
-	 
-	  switch($next){
-	    case 0://go north
-	      $row--;
-	      $grid[$row][$col]=$alphabet[$letter];
-	      break;
-	      
-	    case 1://go west
-	      $col--;
-	      $grid[$row][$col]=$alphabet[$letter];
-	      break;
-	      
-	    case 2://go south
-	      $row++;
-	      $grid[$row][$col]=$alphabet[$letter];
-	      break;
-	      
-	    case 3://go east
-	      $col++;
-	      $grid[$row][$col]=$alphabet[$letter];
-	      break;
-	    }
+ * Function to generate a random walk
+    ********************************************************************************/
+function calculateWalk($gridSize, $walkLength){
+    $seed = mt_rand();
+    mt_srand($seed);
+    echo "Seed = " . $seed . "\n";
+    $first = true;
+    /*Generate a random starting point*/
+    $row = mt_rand() % $gridSize;
+    $col = mt_rand() % $gridSize;
+    $alphabet = str_split("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    $grid[$row][$col] = $alphabet[0]; //set first 'A' in grid
+    for ($n = 0; $n < $walkLength; $n++) { //for as many times as they want the alphabet repeated
+      for ($letter = 0; $letter < 26; $letter++) { //make a path with the alphabet
+        if ($first) { //if this is the first 'A' of all
+          $first = false;
+          $letter = 1;
+        }
+        /*****check whether all sides are blocked******/
+        $taken = checkDirections($row, $col, $gridSize, $grid);
+        /*if they are, print what we have and exit*/
+        if (allTaken($taken)) { //spans through to next php block
+          printGrid($gridSize, $grid);
+          exit(0);
+        } //from previous php block
+        /*********************************************/
+        $next = getNextDirection($taken);
+        switch ($next) {
+          case 0: //go north
+            $row--;
+            $grid[$row][$col] = $alphabet[$letter];
+            break;
+          case 1: //go west
+            $col--;
+            $grid[$row][$col] = $alphabet[$letter];
+            break;
+          case 2: //go south
+            $row++;
+            $grid[$row][$col] = $alphabet[$letter];
+            break;
+          case 3: //go east
+            $col++;
+            $grid[$row][$col] = $alphabet[$letter];
+            break;
+        }
+      }
+    }
+    printGrid($gridSize, $grid);
 }
+
+
 
 ?>
